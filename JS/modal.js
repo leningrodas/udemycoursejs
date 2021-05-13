974435c7824a279574c2710c91de1efab95417a0
lening rodas <lening.a.rodas@gmail.com>
2021-05-13 14:54:55 -0500

    creating the modal and practicing queryselectors

--- JS/modal.js
+++ JS/modal.js
@@ -5,6 +5,32 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
+console.log(btnCloseModal);
 
 for(let i = 0; i< btnsOpenModal.length; i++)
-    console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener('click', function () {
+        console.log('Button clicked');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })
+
+//this takes care of closing modal by creating a function that does both take care of the modal and overlay.
+const closeModal = function (){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+    //one way to do it which is breaking it down in kiss terms.
+//     btnCloseModal.addEventListener('click', function (){
+//         console.log('closed button clicked');
+//         modal.classList.add('hidden');
+//         overlay.classList.add('hidden');
+//     });
+//
+// overlay.addEventListener('click', function (){
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// })
