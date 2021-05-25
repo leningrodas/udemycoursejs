9cdeaf30a88d34cbb43e26650c4d8a2e037d8e8a
lening rodas <lening.a.rodas@gmail.com>
2021-05-25 15:42:51 -0500

    update

--- JS/diceGame.js
+++ JS/diceGame.js
@@ -8,13 +8,16 @@ const diceEl = document.querySelector('.dice');
 const btnnew = document.querySelector('.btn--new');
 const btnroll = document.querySelector('.btn--roll');
 const btnhold = document.querySelector('.btn--hold');
-
+const player0El = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 
 score0EL.textContent = 0;
 scorer1EL.textContent = 0;
 diceEl.classList.add('hidden');
 
 let currentScore = 0;
+let activePlayer = 0;
+const scores = [0, 0];
 
 btnroll.addEventListener('click', function (){
     let dice = Math.trunc(Math.random() * 6) + 1;
@@ -24,9 +27,15 @@ btnroll.addEventListener('click', function (){
 
     if(dice !== 1){
         currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
         current0EL.textContent = currentScore;
     } else {
 
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0;
+        player0El.classList.toggle('player--active');
+        player1EL.classList.toggle('player--active');
     }
 
 })
